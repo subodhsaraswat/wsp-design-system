351dfc6951cb123fc07fffd8f5da103108d19283
wsp-subodh <subodh.sharma2@wipro.com>
2021-10-21 14:18:26 +0530

    home page enhencement

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 function copyToClipboard(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-  }
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+$(document).ready(function () {
+  $(".wsp-navbar-vertical li > a").on("click", function() {
+    $(".wsp-navbar-vertical").find(".active").removeClass("active");
+    $(this).addClass("active");
+    $(".wsp-navbar-vertical").find(".wsp-submenu").slideUp(500); /* close submenu */
+    $(this).next(".wsp-submenu").slideDown(500); /*display submenu */
+  });
+});
