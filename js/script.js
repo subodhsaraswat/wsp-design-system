c64febeb16c1871ae52b21762a000192b9b27cc7
wsp-subodh <subodh.sharma2@wipro.com>
2021-11-24 13:12:52 +0530

    tree added

--- js/script.js
+++ js/script.js
@@ -292,6 +292,57 @@ window.addEventListener('DOMContentLoaded', () => {
 	new dualRangeSlider(document.querySelector(".dual-range"))
 })
 
+/* Tree */
+$.fn.extend({
+  treed: function (o) {
+    
+    var openedClass = 'wsp-icon wsp-icon-folder-minus';
+    var closedClass = 'wsp-icon wsp-icon-folder-plus';
+    
+    if (typeof o != 'undefined'){
+      if (typeof o.openedClass != 'undefined'){
+      openedClass = o.openedClass;
+      }
+      if (typeof o.closedClass != 'undefined'){
+      closedClass = o.closedClass;
+      }
+    };
+    
+      //initialize each of the top levels
+      var tree = $(this);
+      tree.addClass("tree");
+      tree.find('li').has("ul").each(function () {
+          var branch = $(this); //li with children ul
+          branch.prepend("<i class='" + closedClass + "'></i>");
+          branch.addClass('branch');
+          branch.on('click', function (e) {
+              if (this == e.target) {
+                  var icon = $(this).children('i:first');
+                  icon.toggleClass(openedClass + " " + closedClass);
+                  $(this).children().children().toggle();
+              }
+          })
+          branch.children().children().toggle();
+      });
+      //fire event from the dynamically added icon
+    tree.find('.branch .indicator').each(function(){
+      $(this).on('click', function () {
+          $(this).closest('li').click();
+      });
+    });
+      //fire event to open branch if the li contains an anchor instead of text
+      tree.find('.branch>a, .branch>i').each(function () {
+          $(this).on('click', function (e) {
+              $(this).closest('li').click();
+              e.preventDefault();
+          });
+      });
+  }
+});
+
+//Initialization of treeviews
+$('#wspTree').treed({openedClass:'wsp-icon wsp-icon-folder-minus', closedClass:'wsp-icon wsp-icon-folder-plus'});
+
 
 
 
